08c07f81b73e118c9652a266c568b8158bf0d32f
zhenhai123 <33774769+zhenhai123@users.noreply.github.com>
2017-11-19 14:05:53 +0800

    1

---
+++ staticInfo/src/main/java/com/example/staticInfo/StaticInfoApplication.java
@@ -0,0 +1,12 @@
+package com.example.staticInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticInfoApplication.class, args);
+	}
+}
